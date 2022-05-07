632d4a2ff7b42bfb031ae001ddd049dbc067bf97
murabayashi.takumi <takumi@murabayashi.info>
2022-05-07 16:25:42 +0900

    サイト表示の変更

--- app/helpers/product_classes_helper.rb
+++ app/helpers/product_classes_helper.rb
@@ -1,2 +1,10 @@
 module ProductClassesHelper
+  def display_price(range, text, text2, text3 = "-")
+    return text3 if range.blank?
+    if range.max == range.min
+      sprintf(text2, range.min)
+    else
+      sprintf(text, range.min, range.max)
+    end
+  end
 end
